16718bcde1be84daffee5d3d7dde2728330c1473
Ryan Rivera <rrivera@fnal.gov>
2022-01-31 17:45:49 -0600

    Copying tutorial setup_ots.sh as improved demo setup file

--- tools/setup_ots.sh
+++ tools/setup_ots.sh
@@ -5,8 +5,9 @@ sh -c "[ `ps $$ | grep bash | wc -l` -gt 0 ] || { echo 'Please switch to the bas
 echo -e "setup[${LINENO}]: \t ====================================================="
 echo -e "setup[${LINENO}]: \t Initially your products path was PRODUCTS=${PRODUCTS}"
 
+shopt -s expand_aliases #allows for aliases in non-interactive mode (which apparently is critical depending on the temperment of the terminal)
+
 #unalias because the original VM aliased for users
-unalias kx >/dev/null 2>&1
 unalias ots >/dev/null 2>&1
 
 unsetup_all >/dev/null 2>&1
@@ -21,10 +22,11 @@ PRODUCTS=${PWD}/products
 source ${PRODUCTS}/setup
 PRODUCTS=${PWD}/products:/cvmfs/fermilab.opensciencegrid.org/products/artdaq
 
+
 setup mrb
 setup git
 source ${PWD}/localProducts_*/setup
-source mrbSetEnv
+mrbsetenv
 echo -e "setup[${LINENO}]: \t Now your products path is PRODUCTS=${PRODUCTS}"
 echo
 
@@ -37,15 +39,30 @@ ulimit -c unlimited
 #export OTSDAQ_UTILITIES_LIB=${MRB_BUILDDIR}/otsdaq_utilities/lib
 #Done with Setup environment when building with MRB (As there's no setupARTDAQOTS file)
 
+#set number of cores used for compiling, if needed
+if [ "x$CETPKG_J" == "x" ]; then
+	export CETPKG_J=8
+fi
+
 #setup ninja generator
 #============================
-setup ninja v1_8_2
-alias makeninja='pushd $MRB_BUILDDIR; ninja; popd'
-alias mz='mrb z; mrbsetenv; mrb b --generator ninja'
-alias mb='makeninja'
+ninjaver=`ups list -aK+ ninja|sort -V|tail -1|awk '{print $2}'|sed 's|"||g'`
+setup ninja $ninjaver
+#Note: functions survive scripting chaos better than aliases!
+unalias makeninja >/dev/null 2>&1
+function makeninja
+{	pushd $MRB_BUILDDIR; ninja -j $CETPKG_J; popd; }
+# alias makeninja='pushd $MRB_BUILDDIR; ninja -j $CETPKG_J; popd'
+unalias mz >/dev/null 2>&1
+function mz
+{	mrb z; mrbsetenv; mrb b --generator ninja; }
+# alias mz='mrb z; mrbsetenv; mrb b --generator ninja'
+unalias mb >/dev/null 2>&1
+function mb
+{	makeninja; }
+# alias mb='makeninja'
+export OTS_DISABLE_TRACE_DEFAULT=1
 
-export CETPKG_INSTALL=${PWD}/products
-export CETPKG_J=16
 
 export OTS_MAIN_PORT=2015
 
@@ -58,14 +75,51 @@ echo -e "setup[${LINENO}]: \t Now your database path is ARTDAQ_DATABASE_URI \t =
 echo -e "setup[${LINENO}]: \t Now your output data path is OTSDAQ_DATA \t = ${OTSDAQ_DATA}"
 echo
 
-alias rawEventDump="art -c ${PWD}/srcs/otsdaq/artdaq-ots/ArtModules/fcl/rawEventDump.fcl"
-alias kx='ots -k'
+unalias rawEventDump >/dev/null 2>&1
+function rawEventDump
+{	art -c ${PWD}/srcs/otsdaq/artdaq-ots/ArtModules/fcl/rawEventDump.fcl; }
+# alias rawEventDump="art -c ${PWD}/srcs/otsdaq/artdaq-ots/ArtModules/fcl/rawEventDump.fcl"
+
+unalias kx >/dev/null 2>&1
+function kx
+{	
+	echo -e "setup[${LINENO}]: \t Executing ots kill-all..."
+	ots -k; 
+
+    #now hard kill any processes that may be stuck and detached:
+
+	killall -9 art &>/dev/null 2>&1 #hide output
+	killall -9 boardreader &>/dev/null 2>&1 #hide output
+	killall -9 eventbuilder &>/dev/null 2>&1 #hide output
+	killall -9 datalogger &>/dev/null 2>&1 #hide output
+	killall -9 dispatcher &>/dev/null 2>&1 #hide output
+	killall -9 routing_master &>/dev/null 2>&1 #hide output
+	ipcrm -a &>/dev/null 2>&1 #hide output #clean-up shared memory
+	
+	#kills self too:
+	killall -9 ots &>/dev/null 2>&1 #hide output
+	killall -9 xdaq.exe  &>/dev/null 2>&1 #hide output
+	killall -9 otsConsoleFwd  &>/dev/null 2>&1 #hide output
+
+	echo -e "setup[${LINENO}]: \t Done with ots kill-all."
+}
+# alias kx='ots -k'
+
+unalias UpdateOTS.sh >/dev/null 2>&1
+function UpdateOTS.sh
+{	${MRB_SOURCE}/otsdaq_utilities/tools/UpdateOTS.sh $@; }
+# alias UpdateOTS.sh='${MRB_SOURCE}/otsdaq_utilities/tools/UpdateOTS.sh'
 
 echo
-echo -e "setup[${LINENO}]: \t Now use 'ots --wiz' to configure otsdaq"
-echo -e "setup[${LINENO}]: \t\t Then use 'ots' to start otsdaq"
-echo -e "setup[${LINENO}]: \t\t	Or use 'ots --help' for more options"
+echo -e "setup[${LINENO}]: \t Now use 'ots --wiz' to configure otsdaq in wiz(safe) mode"
+echo -e "setup[${LINENO}]: \t         'ots' to start otsdaq in normal mode"
+echo -e "setup[${LINENO}]: \t         'ots --help' for more options"
 echo
-echo -e "setup[${LINENO}]: \t\t use 'kx' to kill otsdaq processes"
+echo -e "setup[${LINENO}]: \t         'kx' to kill all otsdaq processes"
+echo -e "setup[${LINENO}]: \t         'mz' for clean build"
+echo -e "setup[${LINENO}]: \t         'mb' for incremental build"
+echo -e "setup[${LINENO}]: \t         'UpdateOTS.sh' for update options"
 echo
+
+
 
