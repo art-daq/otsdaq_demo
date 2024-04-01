1b354f86fdacf4caf8ed9d4a696dbc54a9e4acdf
Eric Flumerfelt <eflumerf@fnal.gov>
2024-04-01 10:15:47 -0500

    Add a quick-spack-start.sh script, which may be helpful in setting up Spack-based OTS areas

---
+++ tools/quick-spack-start.sh
@@ -0,0 +1,296 @@
+#! /bin/bash
+# quick-mrb-start.sh - Eric Flumerfelt, May 20, 2016
+# Downloads, installs, and runs the artdaq_demo as an MRB-controlled repository
+
+git_status=`git status 2>/dev/null`
+git_sts=$?
+if [ $git_sts -eq 0 ];then
+	echo "This script is designed to be run in a fresh install directory!"
+	exit 1
+fi
+
+starttime=`date`
+Base=$PWD
+test -d log || mkdir log
+test -d Data && rmdir Data
+test -d databases && rmdir databases
+
+env_opts_var=`basename $0 | sed 's/\.sh$//' | tr 'a-z-' 'A-Z_'`_OPTS
+USAGE="\
+   usage: `basename $0` [options] [demo_root]
+examples: `basename $0` .
+		  `basename $0` --run-ots
+		  `basename $0` --debug
+		  `basename $0` --tag v2_08_04
+If the \"demo_root\" optional parameter is not supplied, the user will be
+prompted for this location.
+--run-ots     runs otsdaq
+--debug       perform a debug build
+--develop     Install the develop version of the software (may be unstable!)
+--tag         Install a specific tag of otsdaq
+--spackdir    Install Spack in this directory (or use existing installation)
+-a            Artdaq version number (e.g. 31300 for v3_13_00)
+-s            Use specific qualifiers when building ots
+-v            Be more verbose
+-x            set -x this script
+-w            Check out repositories read/write
+--no-extra-products  Skip the automatic use of central product areas, such as CVMFS
+--upstream    Use <dir> as a Spack upstream (repeatable)
+"
+
+# Process script arguments and options
+eval env_opts=\${$env_opts_var-} # can be args too
+
+spackdir="${SPACK_ROOT:-$Base/spack}"
+upstreams=()
+eval "set -- $env_opts \"\$@\""
+op1chr='rest=`expr "$op" : "[^-]\(.*\)"`   && set -- "-$rest" "$@"'
+op1arg='rest=`expr "$op" : "[^-]\(.*\)"`   && set --  "$rest" "$@"'
+reqarg="$op1arg;"'test -z "${1+1}" &&echo opt -$op requires arg. &&echo "$USAGE" &&exit'
+args= do_help= opt_v=0; opt_w=0; opt_develop=0; opt_skip_extra_products=0; opt_no_pull=0
+while [ -n "${1-}" ];do
+	if expr "x${1-}" : 'x-' >/dev/null;then
+		op=`expr "x$1" : 'x-\(.*\)'`; shift   # done with $1
+		leq=`expr "x$op" : 'x-[^=]*\(=\)'` lev=`expr "x$op" : 'x-[^=]*=\(.*\)'`
+		test -n "$leq"&&eval "set -- \"\$lev\" \"\$@\""&&op=`expr "x$op" : 'x\([^=]*\)'`
+		case "$op" in
+			\?*|h*)     eval $op1chr; do_help=1;;
+			v*)         eval $op1chr; opt_v=`expr $opt_v + 1`;;
+			x*)         eval $op1chr; set -x;;
+			a*)         eval $op1arg; aqualifier=$1; shift;;
+			s*)         eval $op1arg; squalifier=$1; shift;;
+			w*)         eval $op1chr; opt_w=`expr $opt_w + 1`;;
+			-debug)     opt_debug=--debug;;
+            -run-ots)  opt_run_ots=--run-ots;;
+			-develop) opt_develop=1;;
+			-tag)       eval $reqarg; tag=$1; shift;;
+			-spackdir)  eval $op1arg; spackdir=$1; shift;;
+			-no-extra-products)  opt_skip_extra_products=1;;
+			-no-pull)   opt_no_pull=1;;
+			-upstream)  eval $op1arg; upstreams+=($1); shift;;
+			*)          echo "Unknown option -$op"; do_help=1;;
+		esac
+	else
+		aa=`echo "$1" | sed -e"s/'/'\"'\"'/g"` args="$args '$aa'"; shift
+	fi
+done
+eval "set -- $args \"\$@\""; unset args aa
+
+test -n "${do_help-}" -o $# -ge 2 && echo "$USAGE" && exit
+
+if [[ -n "${tag:-}" ]] && [[ $opt_develop -eq 1 ]]; then 
+	echo "The \"--tag\" and \"--develop\" options are incompatible - please specify only one."
+	exit
+fi
+
+if [ "x$SPACK_ROOT" == "x$spackdir" ]; then
+  echo "Using pre-existing Spack installation $SPACK_ROOT.\nIf this is not correct, hit Ctrl-C and run 'unset SPACK_ROOT'."
+  sleep 5
+fi
+
+# JCF, 1/16/15
+# Save all output from this script (stdout + stderr) in a file with a
+# name that looks like "quick-start.sh_Fri_Jan_16_13:58:27.script" as
+# well as all stderr in a file with a name that looks like
+# "quick-start.sh_Fri_Jan_16_13:58:27_stderr.script"
+alloutput_file=$( date | awk -v "SCRIPTNAME=$(basename $0)" '{print SCRIPTNAME"_"$1"_"$2"_"$3"_"$4".script"}' )
+stderr_file=$( date | awk -v "SCRIPTNAME=$(basename $0)" '{print SCRIPTNAME"_"$1"_"$2"_"$3"_"$4"_stderr.script"}' )
+exec  > >(tee "$Base/qms-log/$alloutput_file")
+exec 2> >(tee "$Base/qms-log/$stderr_file")
+
+# Get all the information we'll need to decide which exact flavor of the software to install
+notag=0
+if [ -z "${tag:-}" ]; then 
+  tag=develop;
+  notag=1;
+fi
+wget https://raw.githubusercontent.com/art-daq/otsdaq/$tag/CMakeLists.txt
+demo_version=v`grep "project" $Base/CMakeLists.txt|grep -oE "VERSION [^)]*"|awk '{print $2}'|sed 's/\./_/g'`
+echo "ots Version is $demo_version"
+if [[ $notag -eq 1 ]] && [[ $opt_develop -eq 0 ]]; then
+  tag=$demo_version
+
+  # 06-Mar-2017, KAB: re-fetch the product_deps file based on the tag
+  mv CMakeLists.txt CMakeLists.txt.orig
+  wget https://raw.githubusercontent.com/art-daq/otsdaq/$tag/CMakeLists.txt
+  demo_version=v`grep "project" $Base/CMakeLists.txt|grep -oE "VERSION [^)]*"|awk '{print $2}'|sed 's/\./_/g'`
+  tag=$demo_version
+fi
+
+defaultS="128"
+defaultAD="31300"
+
+if [ -n "${squalifier-}" ]; then
+	squalifier="${squalifier}"
+else
+	squalifier="${defaultS}"
+fi
+if [ -n "${aqualifier-}" ]; then
+	aqualifier="${aqualifier}"
+else
+	aqualifier="${defaultAD}"
+fi
+compiler_info="" # Maybe do e- and c- qualifiers?
+
+if ! [ -d $spackdir ];then
+	$(
+    cd ${spackdir%/spack}
+    git clone https://github.com/FNALssi/spack.git -b fnal-develop
+        )
+fi
+
+export SPACK_DISABLE_LOCAL_CONFIG=true
+source $spackdir/share/spack/setup-env.sh
+
+git clone https://github.com/FNALssi/fermi-spack-tools.git
+./fermi-spack-tools/bin/make_packages_yaml $spackdir
+
+repo_found=`spack repo list|grep -c fnal_art`
+if [ $repo_found -eq 0 ]; then
+    cd $spackdir/var/spack/repos
+    git clone https://github.com/FNALssi/fnal_art.git
+    spack repo add ./fnal_art
+    git clone https://github.com/marcmengel/scd_recipes.git
+    spack repo add ./scd_recipes
+    git clone https://github.com/art-daq/artdaq-spack.git
+    spack repo add ./artdaq-spack
+
+fi
+
+
+spack config --scope=site update  --yes-to-all config
+spack config --scope=site add config:install_tree:padded_length:255
+spack mirror add --scope site fnal https://spack-cache-1.fnal.gov/binaries/
+spack buildcache update-index -k fnal
+spack mirror add --scope site scisoft https://scisoft.fnal.gov/scisoft/spack-mirror/
+spack buildcache update-index -k scisoft
+spack -k buildcache keys --install --trust --force
+spack reindex
+
+for upstream in ${upstreams[@]}; do
+    upstreamdir=`find $upstream -type d -name .spack-db`
+    upstreamdir=`dirname $upstreamdir`
+    
+    if ! [ -d $upstreamdir/.spack-db ]; then
+	   echo "No Spack instance found at $upstream!"
+	   continue
+    fi
+
+    if ! [ -f $spackdir/etc/spack/upstreams.yaml ]; then
+	    echo "upstreams:" > $spackdir/etc/spack/upstreams.yaml
+    fi
+
+    echo "  upstream${upstream//\//-}:" >>$spackdir/etc/spack/upstreams.yaml
+    echo "    install_tree: $upstreamdir" >>$spackdir/etc/spack/upstreams.yaml
+
+done
+
+cd $Base
+spack compiler find
+
+spack env create ots
+spack env activate ots
+
+spack add otsdaq-suite@${demo_version}${compiler_info} s=${squalifier} artdaq=${aqualifier}
+
+	cat >setup_ots.sh <<-EOF
+echo # This script is intended to be sourced.
+
+sh -c "[ \`ps \$\$ | grep bash | wc -l\` -gt 0 ] || { echo 'Please switch to the bash shell before running ots.'; exit; }" || exit
+export SPACK_DISABLE_LOCAL_CONFIG=true
+source $spackdir/share/spack/setup-env.sh
+spack env activate ots
+
+export TRACE_NAME=TRACE
+
+export OTS_MAIN_PORT=2015
+
+export USER_DATA="$Base/Data"
+export ARTDAQ_DATABASE_URI="filesystemdb://$Base/databases/filesystemdb/test_db"
+export OTSDAQ_DATA="$Base/Data/OutputData"
+export OTS_SOURCE=$SPACK_ENV
+
+echo -e "setup [${LINENO}]  \t Now your user data path is USER_DATA \t\t = \${USER_DATA}"
+echo -e "setup [${LINENO}]  \t Now your database path is ARTDAQ_DATABASE_URI \t = \${ARTDAQ_DATABASE_URI}"
+echo -e "setup [${LINENO}]  \t Now your output data path is OTSDAQ_DATA \t = \${OTSDAQ_DATA}"
+echo
+
+alias kx='ots -k'
+
+echo
+echo -e "setup [${LINENO}]  \t Now use 'ots --wiz' to configure otsdaq"
+echo -e "setup [${LINENO}]  \t  	Then use 'ots' to start otsdaq"
+echo -e "setup [${LINENO}]  \t  	Or use 'ots --help' for more options"
+echo
+echo -e "setup [${LINENO}]  \t     use 'kx' to kill otsdaq processes"
+echo
+
+EOF
+#
+
+
+########################################
+########################################
+## Setup USER_DATA and databases
+########################################
+########################################
+cd $Base
+
+# Fetch data and databases from develop
+git clone https://github.com/art-daq/otsdaq_demo -b develop
+if ! [ -d databases ]; then
+    cp -a otsdaq_demo/NoGitDatabases databases
+fi
+if ! [ -d Data ];then 
+    cp -a otsdaq_demo/NoGitData Data
+fi
+rm -rf otsdaq_demo
+
+export USER_DATA="$Base/Data"
+export ARTDAQ_DATABASE_URI="filesystemdb://$Base/databases/filesystemdb/test_db"
+
+		
+#download get_tutorial_data script
+wget https://raw.githubusercontent.com/art-daq/otsdaq_demo/develop/tools/get_tutorial_data.sh -O get_tutorial_data.sh --no-check-certificate
+
+#change permissions so the script is executable
+chmod 755 get_tutorial_data.sh
+
+#execute script
+./get_tutorial_data.sh
+
+
+#copy tutorial launching scripts
+echo
+echo -e "UpdateOTS.sh [${LINENO}]  \t updating tutorial launch scripts..."
+rm get_tutorial_data.sh &>/dev/null 2>&1 #hide output
+rm reset_ots_tutorial.sh &>/dev/null 2>&1 #hide output
+wget https://raw.githubusercontent.com/art-daq/otsdaq_demo/develop/tools/reset_ots_tutorial.sh -O reset_ots_tutorial.sh --no-check-certificate	
+chmod 755 reset_ots_tutorial.sh
+
+
+########################################
+########################################
+## END Setup USER_DATA and databases
+########################################
+########################################	
+	
+spack concretize && spack install
+
+installStatus=$?
+
+if [ $installStatus -eq 0 ]; then
+    echo "otsdaq-demo has been installed correctly. Use 'source setup_ots.sh' to setup your otsdaq software, then follow the instructions or visit the project redmine page for more info: https://github.com/art-daq/otsdaq/wiki"
+    echo	
+	echo "In the future, when you open a new terminal, just use 'source setup_ots.sh' to setup your ots installation."
+	echo
+else
+    echo "BUILD ERROR!!! SOMETHING IS VERY WRONG!!!"
+    echo
+	echo
+fi
+
+endtime=`date`
+
+echo "Build start time: $starttime"
+echo "Build end time:   $endtime"
