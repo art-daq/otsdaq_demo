1401d0e93c3ff53e2bb0026d6e68e3b199db7b44
Eric Flumerfelt <eflumerf@fnal.gov>
2024-11-11 15:21:25 -0600

    Add option to skip creation of a Spack environment view

--- tools/ots-quick-spack-start.sh
+++ tools/ots-quick-spack-start.sh
@@ -37,6 +37,7 @@ prompted for this location.
 --no-extra-products  Skip the automatic use of central product areas, such as CVMFS
 --upstream    Use <dir> as a Spack upstream (repeatable)
 --padding     Set directory padding to 255, for relocatability
+--no-view     Do not create Spack environment views
 --arch        Set architechture for build (ex. linux-almalinux9-x86_64_v3)
 --no-kmod     Do not build TRACE kernel module (for Docker builds)
 "
@@ -51,7 +52,7 @@ eval "set -- $env_opts \"\$@\""
 op1chr='rest=`expr "$op" : "[^-]\(.*\)"`   && set -- "-$rest" "$@"'
 op1arg='rest=`expr "$op" : "[^-]\(.*\)"`   && set --  "$rest" "$@"'
 reqarg="$op1arg;"'test -z "${1+1}" &&echo opt -$op requires arg. &&echo "$USAGE" &&exit'
-args= do_help= opt_v=0; opt_w=0; opt_develop=0; opt_skip_extra_products=0; opt_no_pull=0; opt_padding=0; opt_no_kmod=0
+args= do_help= opt_v=0; opt_w=0; opt_develop=0; opt_skip_extra_products=0; opt_no_pull=0; opt_padding=0; opt_no_kmod=0; opt_no_view=0
 while [ -n "${1-}" ];do
     if expr "x${1-}" : 'x-' >/dev/null;then
         op=`expr "x$1" : 'x-\(.*\)'`; shift   # done with $1
@@ -75,6 +76,7 @@ while [ -n "${1-}" ];do
             -padding)   opt_padding=1;;
             -arch)      eval $op1arg; arch=$1; shift;;
             -no-kmod)   opt_no_kmod=1;;
+            -no-view)   opt_no_view=1;;
             *)          echo "Unknown option -$op"; do_help=1;;
         esac
     else
@@ -144,6 +146,11 @@ compiler_info="" # Maybe do e- and c- qualifiers?
 arch_opt=""
 if [ "x$arch" != "x" ]; then
    arch_opt="arch=$arch"
+fi
+
+view_opt=""
+if [ $opt_no_view -eq 1 ];then
+    view_opt="--without-view"
 fi
 
 if ! [ -d $spackdir ];then
@@ -237,7 +244,7 @@ if [ $? -ne 0 ]; then
 fi
 spack compiler find
 
-spack env create ots-${demo_version}
+spack env create $view_opt ots-${demo_version}
 spack env activate ots-${demo_version}
 ln -s ${spackdir}/var/spack/environments/ots-${demo_version}
 # OTS always wants to re-make the srcs link
