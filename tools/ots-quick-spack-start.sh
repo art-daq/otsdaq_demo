6df73cc7202690e7bd6acf48b6f3582a9ee7470f
Eric Flumerfelt <eflumerf@fnal.gov>
2025-02-28 12:04:44 -0600

    Go back to gcc@13.1.0

--- tools/ots-quick-spack-start.sh
+++ tools/ots-quick-spack-start.sh
@@ -254,11 +254,11 @@ spack reindex
 cd $Base
 
 BUILD_J=$((`cat /proc/cpuinfo|grep processor|tail -1|awk '{print $3}'` + 1))
-spack load --first gcc@13.3.0 >/dev/null 2>&1
+spack load --first gcc@13.1.0 >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-  spack install -j $BUILD_J gcc@13.3.0 ${arch_opt} +binutils
+  spack install -j $BUILD_J gcc@13.1.0 ${arch_opt} +binutils
   installStatus=$?
-  spack load gcc@13.3.0
+  spack load gcc@13.1.0
 fi
 spack compiler find
 
@@ -278,7 +278,7 @@ else
 	spack add trace+kmod
 fi
 
-spack add otsdaq-suite@${demo_version}${compiler_info} s=${squalifier} artdaq=${aqualifier} ${arch_opt} %gcc@13.3.0 +demo
+spack add otsdaq-suite@${demo_version}${compiler_info} s=${squalifier} artdaq=${aqualifier} ${arch_opt} %gcc@13.1.0 +demo
 env_to_activate="ots-${demo_version}"
 
 
@@ -320,7 +320,7 @@ sh -c "[ \`ps \$\$ | grep bash | wc -l\` -gt 0 ] || { echo 'Please switch to the
 export SPACK_DISABLE_LOCAL_CONFIG=true
 source $spackdir/share/spack/setup-env.sh
 
-spack load --first gcc@13.3.0
+spack load --first gcc@13.1.0
 spack compiler find
 
 spack env activate ${env_to_activate}
@@ -445,8 +445,8 @@ if [[ ${opt_develop:-0} -eq 1 ]];then
 	spack env deactivate
 	# spack mpd init # Upstream
     spack mpd init -r site -u $Base/spack-repos/mpd # Fork
-	# spack mpd new-project --force -y --name ots-develop -E ots-${demo_version} cxxstd=20 %gcc@13.3.0 generator=ninja # Upstream
-	spack mpd new-project --force -y --name ots-develop -E ots-${demo_version} cxxstd=20 %gcc@13.3.0 # Fork
+	# spack mpd new-project --force -y --name ots-develop -E ots-${demo_version} cxxstd=20 %gcc@13.1.0 generator=ninja # Upstream
+	spack mpd new-project --force -y --name ots-develop -E ots-${demo_version} cxxstd=20 %gcc@13.1.0 # Fork
 	spack install cetmodules@3.26.00 # Needed for now
 	spack env activate ots-develop
 	spack add cetmodules@3.26.00
