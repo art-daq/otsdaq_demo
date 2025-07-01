c2b1a2fddf38c0678f442e81e17ba0f100054ebb
Eric Flumerfelt <eflumerf@fnal.gov>
2025-07-01 12:55:15 -0500

    Remove the perl-ipc-run3 package from scd_recipes

--- tools/ots-quick-spack-start.sh
+++ tools/ots-quick-spack-start.sh
@@ -198,6 +198,7 @@ if [ $repo_found -eq 0 ]; then
     spack repo add ./fnal_art
     git clone https://github.com/marcmengel/scd_recipes.git
     cd scd_recipes && git checkout e9c8cc8af792008c3c85724cc8ae3ee0662233d6 ; cd ..
+    rm -rf scd_recipes/packages/perl-ipc-run3
     spack repo add ./scd_recipes
     git clone https://github.com/art-daq/artdaq-spack.git
     cd artdaq-spack && git checkout ots-${demo_version}; cd ..
@@ -206,6 +207,7 @@ if [ $repo_found -eq 0 ]; then
 else
     cd fnal_art && git fetch -a && git checkout ddeec355456e3bca5e4a743ce5d4906fa74a51b6 ; cd ..
     cd scd_recipes && git fetch -a && git checkout e9c8cc8af792008c3c85724cc8ae3ee0662233d6 ; cd ..
+    rm -rf scd_recipes/packages/perl-ipc-run3
     cd artdaq-spack && git fetch -a && git checkout ots-${demo_version}; cd ..
     cd $Base
 fi
