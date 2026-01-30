d3d59ac88a0b5a7e73e1d160dc362835ba58771f
Eric Flumerfelt <eflumerf@fnal.gov>
2026-01-30 13:42:37 -0600

    Replace link and versioned script. Update name of spack setup script

--- tools/ots-quick-spack-start_v1.1.sh
+++ tools/ots-quick-spack-start_v1.1.sh
@@ -129,12 +129,12 @@ if [ $opt_no_view -eq 1 ];then
     view_opt="--without-view"
 fi
 
-build_system_script=`find $Base -type f -name setup_spack_build_system_v1.sh`
+build_system_script=`find $Base -type f -name setup_spack_build_system_v1.1.sh`
 if [[ "x$build_system_script" == "x" ]];then
-  echo "WARNING: setup_spack_build_system_v1.sh not found, downloading from https://github.com/art-daq/artdaq-demo"
+  echo "WARNING: setup_spack_build_system_v1.1.sh not found, downloading from https://github.com/art-daq/artdaq-demo"
   cd $Base
-  wget https://raw.githubusercontent.com/art-daq/artdaq_demo/refs/heads/develop/tools/setup_spack_build_system_v1.sh
-  build_system_script=$Base/setup_spack_build_system_v1.sh
+  wget https://raw.githubusercontent.com/art-daq/artdaq_demo/refs/heads/develop/tools/setup_spack_build_system_v1.1.sh
+  build_system_script=$Base/setup_spack_build_system_v1.1.sh
 fi
 source $build_system_script
 # Note that install_spack_build_system sources setup-env.sh
